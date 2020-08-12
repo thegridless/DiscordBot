d9246dd011b652811c93e05d86bff8b7a92016c2
TheGrid <kirill.leonov2015@yandex.by>
2020-08-12 17:03:44 +0300

    verison 0.0.2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import discord
 from discord.ext import commands  # подгрузка библиотек
 
-TOKEN = 'NzQzMDc1MjE1MzEwODQ4MDAw.XzPYuQ.hxq4xbcGqm2ZIz9dyXIVy9Rb8D8'  # токен бота
+TOKEN = 'NzQzMDc1MjE1MzEwODQ4MDAw.XzPYuQ.ksRcVxyBqRGXHWWZ6VemWNZCr5Q'  # токен бота
 
 bot = commands.Bot(command_prefix='!')  # инициализация преффикса
 
