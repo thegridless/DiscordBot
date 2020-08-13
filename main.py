ae6127454bed31ba8aaaab95eec91e4a22927d5c
wakenup <fsoce@mail.ru>
2020-08-13 12:27:36 +0300

    version 0.0.4

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 # Discord Bot # название
-import ctx as ctx
 import typing
 import discord
 from discord.ext import commands  # подгрузка библиотек
 
 TOKEN = 'NzQzMDc1MjE1MzEwODQ4MDAw.XzPYuQ.ksRcVxyBqRGXHWWZ6VemWNZCr5Q'  # токен бота
 
-pcounter = 1 #количество игроков написавших !play
+players =[] #массив игроков
 
 bot = commands.Bot(command_prefix='!')  # инициализация преффикса
 
 
 @bot.command(pass_context=True)  # разрешаем передавать агрументы
 async def play(ctx):#функция для !play
-    global pcounter #использование глобальной переменной pcounter
-    await ctx.send("Number of players " + str(pcounter))
-    pcounter = pcounter + 1
+    if(ctx.author.discriminator in players):
+       await ctx.send("Вы уже в игре")
+    else:
+        players.append(ctx.author.discriminator);
+        global pcounter #использование глобальной переменной pcounter
+        await ctx.send("Игрок " + str(ctx.author) +" присоединился к игре \n" + "Количество игроков : " + str(len(players)))
+        await ctx.send(players)
 
 
 @bot.command()  # правила игры
