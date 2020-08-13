20e66395c250a0ea45fd7c0b4b4e4e58ff583b3d
wakenup <fsoce@mail.ru>
2020-08-13 12:04:05 +0300

    0.0.3

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from discord.ext import commands  # подгрузка библиотек
 
 TOKEN = 'NzQzMDc1MjE1MzEwODQ4MDAw.XzPYuQ.ksRcVxyBqRGXHWWZ6VemWNZCr5Q'  # токен бота
 
+pcounter = 1 #количество игроков написавших !play
+
 bot = commands.Bot(command_prefix='!')  # инициализация преффикса
 
 
 @bot.command(pass_context=True)  # разрешаем передавать агрументы
-async def test(ctx, arg):  # создаем асинхронную фунцию бота
-    await ctx.send(arg)  # отправляем обратно аргумент
+async def play(ctx):#функция для !play
+    global pcounter #использование глобальной переменной pcounter
+    await ctx.send("Number of players " + str(pcounter))
+    pcounter = pcounter + 1
 
 
 bot.run(TOKEN)  # запуск бота//
