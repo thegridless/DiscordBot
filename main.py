b33565efdc6fcc2c9ca118833bcf455897d6df1f
wakenup <fsoce@mail.ru>
2020-08-14 12:44:43 +0300

    version 0.0.6.1

--- main.py
+++ main.py
@@ -20,12 +20,25 @@ async def play(ctx):  # функция для !play
         await ctx.send(str(ctx.author.mention) + ", вы уже в игре")
     else:
         players.append(ctx.author.mention)
-        global pcounter  # использование глобальной переменной pcounter
-        await ctx.send("Игрок " + str(ctx.author.mention) + " присоединился к игре \n" + "Количество игроков : " + str(len(players)))
-        await ctx.send("Список текущих игроков: ")
-        for element in players:
-            await ctx.send(element)
+        # global pcounter  # использование глобальной переменной pcounter
+        # await ctx.send("Игрок " + str(ctx.author.mention) + " присоединился к игре \n" + "Количество игроков : " + str(len(players)))
+        # await ctx.send("Список текущих игроков: ")
+        # for element in players:
+        #     await ctx.send(element)
 
+
+        embed = discord.Embed(
+            title=ctx.author.mention,
+            colour=discord.Colour.blue()
+        )
+
+        embed.set_footer(text='Хорошей игры')
+        embed.set_image(url='https://2ch.hk/b/arch/2020-07-07/src/224156532/15940650663840.png')
+        embed.set_thumbnail(url='https://2ch.hk/b/arch/2020-07-07/src/224156532/15940650663840.png')
+        embed.add_field(name="Количетсво участников: ",value=str(len(players)),inline=True)
+        embed.add_field(name='Список участников',value=str(players),inline=False)
+
+        await ctx.send(embed=embed)
 
 #функция для того чтобы покинуть игру
 @bot.command()
